b6c4a7431480c198d9a0ab4754128ac5a7a36861
Patrick ONeill <poneill@sembracare.com>
2019-11-24 17:20:59 -0500

    adding shift view

--- public/js/viewShift.js
+++ public/js/viewShift.js
@@ -7,8 +7,20 @@ $(document).ready(function() {
       url: "/api/shiftRecord/" + userData.id,
       type: "GET"
     }).then(function(shift) {
-      var shiftTasks = shift.shiftRecord;
-      console.log(JSON.stringify(shiftTasks));
+      var shiftTasks = JSON.parse(shift.shiftRecord);
+      Object.keys(shiftTasks).forEach(function(category) {
+        var shiftCategory = shiftTasks[category];
+        console.log(shiftCategory);
+        Object.keys(shiftCategory).forEach(function(task) {
+          var accordianHeader = "title" + category;
+          $("#" + accordianHeader).text(
+            category + " (" + Object.keys(shiftCategory).length + ")"
+          );
+          var accordianBody = "tasks" + category;
+          var taskSection = $("#" + accordianBody);
+          taskSection.append(task);
+        });
+      });
     });
   });
 });
