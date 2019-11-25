fe6b25b22af0248a00616fd253c420107264cf1a
Jamie ONeill <joneill@sembracare.com>
2019-11-25 18:49:47 -0500

    adding first signature box......note everyone needs to do npm i in CLP

---
+++ public/js/signature_pad.js
@@ -0,0 +1,17 @@
+var signaturePad = new SignaturePad(document.getElementById("signature-pad"), {
+  backgroundColor: "rgba(255, 255, 255, 0)",
+  penColor: "rgb(0, 0, 0)"
+});
+var saveButton = document.getElementById("save");
+var cancelButton = document.getElementById("clear");
+
+saveButton.addEventListener("click", function() {
+  var data = signaturePad.toDataURL("image/png");
+
+  // Send data to server instead...
+  window.open(data);
+});
+
+cancelButton.addEventListener("click", function() {
+  signaturePad.clear();
+});
