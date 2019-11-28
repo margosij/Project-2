f97b046b76234d4585436eb176dcbaf6a2371ab1
Patrick ONeill <poneill@sembracare.com>
2019-11-28 14:58:24 -0500

    adding icomplete task functionality

--- public/js/viewShift.js
+++ public/js/viewShift.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
   var userId;
-
+  var incompleteButton;
   $(document).on("click", "button.complete", toggleComplete);
+  $(document).on("click", "button.incomplete", toggleIncompleteModal);
   $(document).on("click", "#btnEmployeeSign", btnEmployeeSign);
+  $(document).on("click", "#modalIncompleteSave", toggleIncomplete);
   $("#btnEmployeeSign").hide();
   $("#rowFinalize").hide();
+  $("#modalError").hide();
   // Toggles complete status
   function toggleComplete(event) {
     event.stopPropagation();
@@ -25,8 +28,14 @@ $(document).ready(function() {
         $(this)
           .parent()
           .css("background-color", "white");
-        $(this).removeAttr(category + "checked");
-        $(this).removeAttr("complete");
+        $(this)
+          .parent()
+          .find("button")
+          .removeAttr(category + "checked");
+        $(this)
+          .parent()
+          .find("button")
+          .removeAttr("complete");
       } else {
         return;
       }
@@ -76,14 +85,110 @@ $(document).ready(function() {
         url: "/api/shiftRecord",
         data: shiftUpdate
       }).then(function() {
-        var tasksTotal = $("li button").length;
+        var tasksTotal = $("li button").length / 2;
         var tasksCompleted = $("li button[complete]").length;
 
         if (tasksCompleted === tasksTotal) {
           $("#btnEmployeeSign").show();
         }
+
+        $("#dropdownReasonIncomplete").val("Select...");
+        $("#tbReasonIncomplete").val("");
       });
     });
+  }
+
+  function toggleIncompleteModal(event) {
+    event.stopPropagation();
+    incompleteButton = $(this);
+    var category = $(this).data("adl");
+    if (
+      incompleteButton
+        .parent()
+        .find("span")
+        .css("text-decoration-line") === "line-through"
+    ) {
+      if (confirm("Are you sure you want to undo incomplete/refused task?")) {
+        incompleteButton
+          .parent()
+          .find("span")
+          .css("text-decoration", "none");
+        incompleteButton.parent().css("background-color", "white");
+        incompleteButton
+          .parent()
+          .find("button")
+          .removeAttr(category + "checked");
+        incompleteButton
+          .parent()
+          .find("button")
+          .removeAttr("complete");
+      } else {
+        return;
+      }
+
+      var totalADL = $("#tasks" + category + " li").length;
+      var completeADL = $(
+        "#tasks" + category + " li button[" + category + "checked]"
+      ).length;
+      $("#title" + category).text(
+        category + " (" + completeADL + " of " + totalADL + ")"
+      );
+
+      var taskKey = $(this)
+        .parent()
+        .find("span")
+        .text();
+      updateShiftRecord(category, taskKey, "");
+    } else {
+      $("#lblTaskCategory").text($(this).data("adl"));
+      $("#lblTaskKey").text(
+        $(this)
+          .parent()
+          .find("span")
+          .text()
+      );
+
+      $("#modalIncomplete").modal("toggle");
+    }
+  }
+
+  // Toggles complete status
+  function toggleIncomplete(event) {
+    event.stopPropagation();
+    if (
+      $("#dropdownReasonIncomplete").val() !== "Select..." &&
+      $("#tbReasonIncomplete").val() !== ""
+    ) {
+      $("#modalError").hide();
+      $("#modalIncomplete").modal("toggle");
+      var taskValue = "";
+      var category = $("#lblTaskCategory").text();
+
+      incompleteButton
+        .parent()
+        .find("span")
+        .css("text-decoration", "line-through");
+      incompleteButton.parent().css("background-color", "red");
+      incompleteButton.attr(category + "checked", "true");
+      incompleteButton.attr("complete", "true");
+      taskValue =
+        $("#dropdownReasonIncomplete").val() +
+        " - " +
+        $("#tbReasonIncomplete").val();
+
+      var totalADL = $("#tasks" + category + " li").length;
+      var completeADL = $(
+        "#tasks" + category + " li button[" + category + "checked]"
+      ).length;
+      $("#title" + category).text(
+        category + " (" + completeADL + " of " + totalADL + ")"
+      );
+
+      var taskKey = $("#lblTaskKey").text();
+      updateShiftRecord(category, taskKey, taskValue);
+    } else {
+      $("#modalError").show();
+    }
   }
 
   function btnEmployeeSign() {
@@ -110,7 +215,10 @@ $(document).ready(function() {
           var newTaskItem = $(
             [
               "<li class='list-group-item d-flex justify-content-between align-items-center task-item'>",
-              "<span>",
+              "<button class='incomplete btn btn-danger' data-adl='" +
+                category +
+                "'>x</button>",
+              "<span class='text-center'>",
               task,
               "</span>",
               "<button class='complete btn btn-primary' data-adl='" +
@@ -126,6 +234,13 @@ $(document).ready(function() {
             newTaskItem.find("button").attr(category + "checked", "true");
             newTaskItem.find("button").attr("complete", "true");
             newTaskItem.css("background-color", "green");
+          } else if (shiftCategory[task] !== "") {
+            newTaskItem
+              .find("span")
+              .css("text-decoration-line", "line-through");
+            newTaskItem.find("button").attr(category + "checked", "true");
+            newTaskItem.find("button").attr("complete", "true");
+            newTaskItem.css("background-color", "red");
           }
 
           var accordianBody = "tasks" + category;
@@ -133,9 +248,9 @@ $(document).ready(function() {
           taskSection.append(newTaskItem);
 
           var totalADL = $("#tasks" + category + " li").length;
-          var completeADL = $(
-            "#tasks" + category + " li button[" + category + "checked]"
-          ).length;
+          var completeADL =
+            $("#tasks" + category + " li button[" + category + "checked]")
+              .length / 2;
           $("#title" + category).text(
             category + " (" + completeADL + " of " + totalADL + ")"
           );
