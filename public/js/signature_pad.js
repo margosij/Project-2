49f8b0e8dff8f9408a775f86ebc45be5263b23b4
Patrick ONeill <poneill@sembracare.com>
2019-11-28 12:59:53 -0500

    adding client signature and shift finalize section

--- public/js/signature_pad.js
+++ public/js/signature_pad.js
@@ -2,16 +2,81 @@ var signaturePad = new SignaturePad(document.getElementById("signature-pad"), {
   backgroundColor: "rgba(255, 255, 255, 0)",
   penColor: "rgb(0, 0, 0)"
 });
+var signaturePad2 = new SignaturePad(
+  document.getElementById("signature-pad2"),
+  {
+    backgroundColor: "rgba(255, 255, 255, 0)",
+    penColor: "rgb(0, 0, 0)"
+  }
+);
 var saveButton = document.getElementById("save");
 var cancelButton = document.getElementById("clear");
+var saveButton2 = document.getElementById("save2");
+var cancelButton2 = document.getElementById("clear2");
+var shiftId = $("#shiftId").val();
 
-saveButton.addEventListener("click", function() {
+saveButton.addEventListener("click", saveSignature);
+cancelButton.addEventListener("click", clearSignature);
+saveButton2.addEventListener("click", saveSignature2);
+cancelButton2.addEventListener("click", clearSignature2);
+
+$.ajax({
+  url: "/api/user_data",
+  type: "GET"
+}).then(function(userData) {
+  userId = userData.id;
+  $.ajax({
+    url: "/api/shiftRecord/" + userId,
+    type: "GET"
+  }).then(function(shift) {
+    if (shift.clientSignature) {
+      signaturePad2.fromDataURL(shift.clientSignature);
+      signaturePad2.off();
+    }
+  });
+});
+
+function clearSignature() {
+  signaturePad.clear();
+}
+function clearSignature2() {
+  signaturePad2.clear();
+}
+
+function saveSignature() {
   var data = signaturePad.toDataURL("image/png");
 
-  // Send data to server instead...
-  window.open(data);
-});
+  var shiftUpdate = {
+    id: shiftId,
+    employeeSignature: data
+  };
 
-cancelButton.addEventListener("click", function() {
-  signaturePad.clear();
-});
+  $.ajax({
+    method: "PUT",
+    url: "/api/shiftEmployeeSignature",
+    data: shiftUpdate
+  }).then(function() {
+    $(".ui-accordion-content").show();
+    $("#btnEmployeeSign").hide();
+    $("#rowFinalize").show();
+    $("#modalMatch").modal("toggle");
+  });
+}
+
+function saveSignature2() {
+  var data = signaturePad2.toDataURL("image/png");
+
+  var shiftUpdate = {
+    id: $("#shiftId").val(),
+    clientSignature: data
+  };
+  // Send data to server instead...
+  //window.open(data);
+  $.ajax({
+    method: "PUT",
+    url: "/api/shiftClientSignature",
+    data: shiftUpdate
+  }).then(function() {
+    window.location.href = "./home.html";
+  });
+}
--- public/js/viewShift.js
+++ public/js/viewShift.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
-  $(document).on("click", "button.complete", toggleComplete);
+  var userId;
 
+  $(document).on("click", "button.complete", toggleComplete);
+  $(document).on("click", "#btnEmployeeSign", btnEmployeeSign);
+  $("#btnEmployeeSign").hide();
+  $("#rowFinalize").hide();
   // Toggles complete status
   function toggleComplete(event) {
     event.stopPropagation();
+    var taskValue = "";
+    var category = $(this).data("adl");
+
     if (
       $(this)
         .parent()
@@ -15,49 +22,132 @@ $(document).ready(function() {
           .parent()
           .find("span")
           .css("text-decoration", "none");
+        $(this)
+          .parent()
+          .css("background-color", "white");
+        $(this).removeAttr(category + "checked");
+        $(this).removeAttr("complete");
+      } else {
+        return;
       }
     } else {
       $(this)
         .parent()
         .find("span")
         .css("text-decoration", "line-through");
+      $(this)
+        .parent()
+        .css("background-color", "green");
+      $(this).attr(category + "checked", "true");
+      $(this).attr("complete", "true");
+      taskValue = "C";
     }
+
+    var totalADL = $("#tasks" + category + " li").length;
+    var completeADL = $(
+      "#tasks" + category + " li button[" + category + "checked]"
+    ).length;
+    $("#title" + category).text(
+      category + " (" + completeADL + " of " + totalADL + ")"
+    );
+
+    var taskKey = $(this)
+      .parent()
+      .find("span")
+      .text();
+    updateShiftRecord(category, taskKey, taskValue);
+  }
+
+  function updateShiftRecord(category, taskKey, taskValue) {
+    $.ajax({
+      url: "/api/shiftRecord/" + userId,
+      type: "GET"
+    }).then(function(shift) {
+      var shiftTasks = JSON.parse(shift.shiftRecord);
+      shiftTasks[category][taskKey] = taskValue;
+
+      var shiftUpdate = {
+        shiftRecord: JSON.stringify(shiftTasks),
+        id: shift.id
+      };
+
+      $.ajax({
+        method: "PUT",
+        url: "/api/shiftRecord",
+        data: shiftUpdate
+      }).then(function() {
+        var tasksTotal = $("li button").length;
+        var tasksCompleted = $("li button[complete]").length;
+
+        if (tasksCompleted === tasksTotal) {
+          $("#btnEmployeeSign").show();
+        }
+      });
+    });
+  }
+
+  function btnEmployeeSign() {
+    $("#modalMatch").modal("toggle");
   }
 
   $.ajax({
     url: "/api/user_data",
     type: "GET"
   }).then(function(userData) {
+    userId = userData.id;
     $.ajax({
-      url: "/api/shiftRecord/" + userData.id,
+      url: "/api/shiftRecord/" + userId,
       type: "GET"
     }).then(function(shift) {
+      $("#shiftId").val(shift.id);
+      $("#lblShiftDate").text("Shift Date: " + shift.shiftDate);
+      $("#lblShiftCheckIn").text("Shift Start: " + shift.shiftStartTime);
+      $("#lblShiftCheckOut").text("Shift End: " + shift.shiftEndTime);
       var shiftTasks = JSON.parse(shift.shiftRecord);
       Object.keys(shiftTasks).forEach(function(category) {
         var shiftCategory = shiftTasks[category];
         Object.keys(shiftCategory).forEach(function(task) {
-          var accordianHeader = "title" + category;
-          $("#" + accordianHeader).text(
-            category + " (" + Object.keys(shiftCategory).length + ")"
-          );
-
           var newTaskItem = $(
             [
               "<li class='list-group-item d-flex justify-content-between align-items-center task-item'>",
               "<span>",
               task,
               "</span>",
-              //"<button class='delete btn btn-danger'>x</button>",
-              "<button class='complete btn btn-primary'>✓</button>",
+              "<button class='complete btn btn-primary' data-adl='" +
+                category +
+                "'>✓</button>",
               "</li>"
             ].join("")
           );
+          if (shiftCategory[task] === "C") {
+            newTaskItem
+              .find("span")
+              .css("text-decoration-line", "line-through");
+            newTaskItem.find("button").attr(category + "checked", "true");
+            newTaskItem.find("button").attr("complete", "true");
+            newTaskItem.css("background-color", "green");
+          }
 
           var accordianBody = "tasks" + category;
           var taskSection = $("#" + accordianBody);
           taskSection.append(newTaskItem);
+
+          var totalADL = $("#tasks" + category + " li").length;
+          var completeADL = $(
+            "#tasks" + category + " li button[" + category + "checked]"
+          ).length;
+          $("#title" + category).text(
+            category + " (" + completeADL + " of " + totalADL + ")"
+          );
         });
       });
+
+      if (shift.clientSignature) {
+        $("li").addClass("disabled");
+        $("#rowFinalize").show();
+        $("#save2").hide();
+        $("#clear2").hide();
+      }
     });
   });
 });
